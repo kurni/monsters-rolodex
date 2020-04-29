4a2b19abbf6fe3fc03755271ffd52fed5624dd80
Kurniawan Wijaya <kurniawan.wijaya@gmail.com>
2020-04-29 19:59:02 +1000

    Updates

---
+++ precache-manifest.c249574b0af4fa4ebd13fe3c8cbe54b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51d5a6f3a9a20059b0edd388a2681c8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c81a321e135443cc6f14",
+    "url": "/monsters-rolodex/static/css/main.1174ab6e.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81a321e135443cc6f14",
+    "url": "/monsters-rolodex/static/js/main.89efd5c8.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
